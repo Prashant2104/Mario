260a033dab94eadea3400ca05cc71b7b6275a646
Prashant9212 <Pacific9212@users.noreply.github.com>
2021-02-23 11:22:09 +0530

    added movements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10f;
+    public float jumpspeed = 5f;
+    private bool Isgrounded = true;
     private Rigidbody2D rigidBody2D;
     private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
@@ -18,8 +20,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         var h = Input.GetAxis("Horizontal");
-
         rigidBody2D.velocity = new Vector2(h * speed, rigidBody2D.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && Isgrounded == true)
+        {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpspeed);
+        }
 
         if (h < 0)
         {
@@ -31,5 +37,15 @@ public class Player : MonoBehaviour
             //transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * 1, transform.localScale.y, transform.localScale.z);
             spriteRenderer.flipX = false;
         }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Base")
+            Isgrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Base")
+            Isgrounded = false;
     }
 }
