d4dd5c9dbc3d6f38e61bee317a0f737e49d00726
Prashant9212 <Pacific9212@users.noreply.github.com>
2021-02-23 10:58:20 +0530

    added movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,20 +18,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         var h = Input.GetAxis("Horizontal");
-        //var v = Input.GetAxis("Vertical");
 
         rigidBody2D.velocity = new Vector2(h * speed, rigidBody2D.velocity.y);
-        //rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, v * speed);
 
         if (h < 0)
         {
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
-            //spriteRenderer.flipX = true;
+            //transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
+            spriteRenderer.flipX = true;
         }
         else if(h > 0)
         {
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * 1, transform.localScale.y, transform.localScale.z);
-            //spriteRenderer.flipX = false;
+            //transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * 1, transform.localScale.y, transform.localScale.z);
+            spriteRenderer.flipX = false;
         }
     }
 }
